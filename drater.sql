e75f19fd95517f3e7dcc3b41395a344367ded89d
ClementMoreauEPSI <aclemoro@free.fr>
2020-02-26 18:10:48 +0100

    Modification base de donnée

--- drater.sql
+++ drater.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3308
--- Généré le :  sam. 22 fév. 2020 à 13:28
+-- Généré le :  mer. 26 fév. 2020 à 17:10
 -- Version du serveur :  8.0.18
--- Version de PHP :  7.4.2
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,11 +30,18 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `classe`;
 CREATE TABLE IF NOT EXISTS `classe` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` bigint(11) NOT NULL AUTO_INCREMENT,
   `Libelle` varchar(255) NOT NULL,
-  `promo` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `promo` datetime NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `classe`
+--
+
+INSERT INTO `classe` (`Id`, `Libelle`, `promo`) VALUES
+(1, 'B2', '2019-09-02 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -50,15 +57,18 @@ CREATE TABLE IF NOT EXISTS `eleve` (
   `mdp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `IdClasse` bigint(20) NOT NULL,
   `Photo_Profile` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`Id`),
+  KEY `IdClasse` (`IdClasse`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `eleve`
 --
 
 INSERT INTO `eleve` (`Id`, `pseudo`, `mail`, `mdp`, `IdClasse`, `Photo_Profile`) VALUES
-(1, 'Clement', 'clement.moreau@epsi.fr', 'clement', 1, 'clement.jpg');
+(2, 'math', 'mathnewph@gmail.com', 'math', 1, 'https://picsum.photos/200'),
+(4, 'clement', 'clement', 'clement', 1, 'Kernel Power 41.PNG'),
+(5, 'clement', 'clement', 'clement', 1, 'Kernel Power 41.PNG');
 
 -- --------------------------------------------------------
 
@@ -72,16 +82,107 @@ CREATE TABLE IF NOT EXISTS `retard` (
   `titre` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `file` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `idEleve` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idCreateur` (`idEleve`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `retard`
 --
 
-INSERT INTO `retard` (`id`, `titre`, `description`, `file`) VALUES
-(1, 'La police m\'a arrêté', 'Malheureusement c\'est la vie', ''),
-(2, 'Mon chien le salaud', 'Mon chient est un petit salaud. Il a mangé ma voiture. Petit chenapan va!', 'Oui');
+INSERT INTO `retard` (`id`, `titre`, `description`, `file`, `idEleve`) VALUES
+(4, 'La police m\'arrête, c\'est relou', 'Malheureusement la police m\'arrête c\'est relou, mais bon, c\'est la vie.', 'https://picsum.photos/200', 2),
+(17, 'sdqsd', 'qdqsd', 'Kernel Power 41.PNG', 2),
+(18, 'Test', 'qsdqsd', '', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `tags`
+--
+
+INSERT INTO `tags` (`id`, `libelle`) VALUES
+(1, 'Insolite'),
+(2, 'WTF'),
+(3, 'Idiot'),
+(4, 'Surnaturel'),
+(5, 'TestTAg'),
+(6, 'qdqsd');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tags_retard`
+--
+
+DROP TABLE IF EXISTS `tags_retard`;
+CREATE TABLE IF NOT EXISTS `tags_retard` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idRetard` int(11) NOT NULL,
+  `idTags` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `tags_retard`
+--
+
+INSERT INTO `tags_retard` (`id`, `idRetard`, `idTags`) VALUES
+(4, 17, 2),
+(5, 18, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vote`
+--
+
+DROP TABLE IF EXISTS `vote`;
+CREATE TABLE IF NOT EXISTS `vote` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idEleve` bigint(20) NOT NULL,
+  `idRetard` int(11) NOT NULL,
+  `dateVote` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `valeur` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idEleve` (`idEleve`),
+  UNIQUE KEY `idRetard` (`idRetard`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `eleve`
+--
+ALTER TABLE `eleve`
+  ADD CONSTRAINT `eleve_ibfk_1` FOREIGN KEY (`IdClasse`) REFERENCES `classe` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `retard`
+--
+ALTER TABLE `retard`
+  ADD CONSTRAINT `retard_ibfk_1` FOREIGN KEY (`idEleve`) REFERENCES `eleve` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `vote`
+--
+ALTER TABLE `vote`
+  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`idRetard`) REFERENCES `retard` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`idEleve`) REFERENCES `eleve` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
